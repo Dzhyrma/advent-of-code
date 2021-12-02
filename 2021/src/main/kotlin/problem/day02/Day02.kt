a78df87d339a92c418fead3501a87485203e0592
Andrii Dzhyrma <andrii.dzhyrma@external.erstegroup.com>
2021-12-02 06:37:38 +0100

    Add day 2 solution

--- 2021/src/main/kotlin/problem/day02/Day02.kt
+++ 2021/src/main/kotlin/problem/day02/Day02.kt
@@ -13,9 +13,47 @@ fun main(args: Array<String>) {
 
 
 fun solveDay02Part1(input: List<String>): Int {
-    TODO()
+    var horizontalPosition = 0
+    var depth = 0
+    navigateSubmarine(
+        input,
+        forwardAction = { horizontalPosition += it },
+        downAction = { depth += it },
+        upAction = { depth -= it },
+    )
+    return horizontalPosition * depth
 }
 
 fun solveDay02Part2(input: List<String>): Int {
-    TODO()
+    var aim = 0
+    var horizontalPosition = 0
+    var depth = 0
+    navigateSubmarine(
+        input,
+        forwardAction = {
+            horizontalPosition += it
+            depth += it * aim
+        },
+        downAction = { aim += it },
+        upAction = { aim -= it },
+    )
+    return horizontalPosition * depth
+}
+
+private fun navigateSubmarine(
+    input: List<String>,
+    forwardAction: (Int) -> Unit,
+    downAction: (Int) -> Unit,
+    upAction: (Int) -> Unit,
+) {
+    input.stream()
+        .map { it.split(' ').let { list -> list[0] to list[1].toInt() } }
+        .forEach { (direction, distance) ->
+            when (direction) {
+                "forward" -> forwardAction(distance)
+                "down" -> downAction(distance)
+                "up" -> upAction(distance)
+                else -> error("Input is wrong")
+            }
+        }
 }
