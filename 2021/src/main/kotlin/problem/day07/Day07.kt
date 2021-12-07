0458bfc9f556358f1f3cd6c6ad499e3845f50243
Andrii Dzhyrma <dzhyrma@gmail.com>
2021-12-07 07:47:38 +0100

    Add day 7 solution

--- 2021/src/main/kotlin/problem/day07/Day07.kt
+++ 2021/src/main/kotlin/problem/day07/Day07.kt
@@ -3,6 +3,7 @@ package problem.day07
 import common.InputRepo
 import common.readSessionCookie
 import common.solve
+import kotlin.math.abs
 
 fun main(args: Array<String>) {
     val day = 7
@@ -13,9 +14,17 @@ fun main(args: Array<String>) {
 
 
 fun solveDay07Part1(input: List<String>): Int {
-    TODO()
+    return findMinimalFuelConsumption(input) { x1, x2 -> abs(x1 - x2) }
 }
 
 fun solveDay07Part2(input: List<String>): Int {
-    TODO()
+    return findMinimalFuelConsumption(input) { x1, x2 -> abs(x1 - x2).let { avg -> (avg + 1) * avg / 2 } }
+}
+
+private fun findMinimalFuelConsumption(input: List<String>, fuelCalculator: (Int, Int) -> Int): Int {
+    return input.first().split(',').map { it.toInt() }.let { coordinates ->
+        ((coordinates.minOrNull() ?: 0)..(coordinates.maxOrNull() ?: 0)).minOfOrNull { caveCoordinate ->
+            coordinates.sumOf { fuelCalculator(it, caveCoordinate) }
+        } ?: 0
+    }
 }
