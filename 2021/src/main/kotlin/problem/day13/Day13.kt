80c6286960812a7639dc5dad65b4de879e6d7f4f
Andrii Dzhyrma <dzhyrma@gmail.com>
2021-12-13 12:23:22 +0100

    Add day 13 solution

--- 2021/src/main/kotlin/problem/day13/Day13.kt
+++ 2021/src/main/kotlin/problem/day13/Day13.kt
@@ -13,9 +13,53 @@ fun main(args: Array<String>) {
 
 
 fun solveDay13Part1(input: List<String>): Int {
-    TODO()
+    val (paper, folds) = convertInput(input)
+
+    return foldPaper(paper, folds.first().first, folds.first().second).size
 }
 
 fun solveDay13Part2(input: List<String>): Int {
-    TODO()
+    val (paper, folds) = convertInput(input)
+
+    return folds.fold(paper) { foldedPaper, (foldAxis, foldIndex) ->
+        foldPaper(foldedPaper, foldAxis, foldIndex)
+    }.also {
+        repeat(6) { y ->
+            repeat(40) { x ->
+                print(if (it.contains(x to y)) "##" else "  ")
+            }
+            println()
+        }
+    }.size
+}
+
+private fun convertInput(input: List<String>): Pair<Set<Pair<Int, Int>>, List<Pair<String, Int>>> {
+    return input.indexOf("")
+        .let { input.take(it) to input.drop(it + 1) }
+        .let { (coordinates, folds) ->
+            coordinates.map { line -> line.split(',').map { it.toInt() }.let { it[0] to it[1] } }.toSet() to
+                    folds.map { line -> line.drop(11).split("=").let { it[0] to it[1].toInt() } }
+        }
+}
+
+private fun foldPaper(
+    paper: Set<Pair<Int, Int>>,
+    foldAxis: String,
+    foldIndex: Int,
+) = when (foldAxis) {
+    "x" -> paper.mapNotNull { (x, y) ->
+        when {
+            x < foldIndex -> x to y
+            x == foldIndex -> null
+            else -> (2 * foldIndex - x) to y
+        }
+    }.toSet()
+    "y" -> paper.mapNotNull { (x, y) ->
+        when {
+            y < foldIndex -> x to y
+            y == foldIndex -> null
+            else -> x to (2 * foldIndex - y)
+        }
+    }.toSet()
+    else -> error("Wrong input")
 }
