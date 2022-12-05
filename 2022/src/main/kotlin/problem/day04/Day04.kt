44003e992d8dd2f44093123e183e5dff5a3538d3
Andrii Dzhyrma <andrii.dzhyrma@external.erstegroup.com>
2022-12-05 07:42:24 +0100

    Add 2022 day 4 solution

--- 2022/src/main/kotlin/problem/day04/Day04.kt
+++ 2022/src/main/kotlin/problem/day04/Day04.kt
@@ -3,6 +3,8 @@ package problem.day04
 import common.InputRepo
 import common.readSessionCookie
 import common.solve
+import java.lang.Integer.max
+import java.lang.Integer.min
 
 fun main(args: Array<String>) {
     val day = 4
@@ -13,9 +15,22 @@ fun main(args: Array<String>) {
 
 
 fun solveDay04Part1(input: List<String>): Int {
-    TODO()
+    return elvesAssignedRanges(input)
+        .count { (firstRange, secondRange) -> firstRange.contains(secondRange) || secondRange.contains(firstRange) }
 }
 
 fun solveDay04Part2(input: List<String>): Int {
-    TODO()
+    return elvesAssignedRanges(input)
+        .count { (firstRange, secondRange) -> doRangeOverlap(firstRange, secondRange) }
 }
+
+private fun elvesAssignedRanges(input: List<String>) = input
+    .map { it.substringBefore(',') to it.substringAfter(',') }
+    .map { (firstElf, secondElf) -> firstElf.toRange() to secondElf.toRange() }
+
+private fun String.toRange() = substringBefore('-').toInt()..substringAfter('-').toInt()
+
+private fun IntRange.contains(other: IntRange) = first <= other.first && last >= other.last
+private fun doRangeOverlap(first: IntRange, second: IntRange) =
+    min(first.last, second.last) - max(first.first, second.first) >= 0
+
--- 2022/src/test/kotlin/problem/day04/Day04Test.kt
+++ 2022/src/test/kotlin/problem/day04/Day04Test.kt
@@ -6,11 +6,17 @@ import io.kotest.matchers.shouldBe
 class Day04Test : FreeSpec({
 
     val sampleInput: List<String> = listOf(
+        "2-4,6-8",
+        "2-3,4-5",
+        "5-7,7-9",
+        "2-8,3-7",
+        "6-6,4-6",
+        "2-6,4-8",
     )
 
-    val sampleSolutionPart1: Int = -1
+    val sampleSolutionPart1 = 2
 
-    val sampleSolutionPart2: Int = -1
+    val sampleSolutionPart2 = 4
 
     "Solving day 4" - {
         "part 1 for the sample input should return the correct output" {
