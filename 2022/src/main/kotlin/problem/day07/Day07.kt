bbbbf5eb0fa60ed5d3abd58c2351ebcadce2468a
Andrii Dzhyrma <andrii.dzhyrma@external.erstegroup.com>
2022-12-07 07:46:12 +0100

    Add 2022 day 7 solution

--- 2022/src/main/kotlin/problem/day07/Day07.kt
+++ 2022/src/main/kotlin/problem/day07/Day07.kt
@@ -11,11 +11,55 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay07Part1, ::solveDay07Part2)
 }
 
+data class Directory(val name: String, val parent: Directory? = null) {
+    val files = mutableListOf<File>()
+    val directories = mutableListOf<Directory>()
+
+    val allSubDirectories: List<Directory>
+        get() = directories.fold(directories.toList()) { acc, directory -> acc + directory.allSubDirectories }
+
+    fun totalSize(): Int = files.sumOf { it.size } + directories.sumOf { it.totalSize() }
+}
+
+data class File(val name: String, val size: Int)
 
 fun solveDay07Part1(input: List<String>): Int {
-    TODO()
+    val root = Directory("/")
+
+    process(input, root)
+
+    return root.allSubDirectories.map { it.totalSize() }.filter { it <= 100000 }.sum()
 }
 
 fun solveDay07Part2(input: List<String>): Int {
-    TODO()
+    val root = Directory("/")
+
+    process(input, root)
+
+    val unusedSpace = 70000000 - root.totalSize()
+    val requiredSpace = 30000000
+
+    return root.allSubDirectories.map { it.totalSize() }.filter { it >= requiredSpace - unusedSpace }.min()
+}
+
+private fun process(input: List<String>, root: Directory) {
+    var current = root
+
+    input.forEach { line ->
+        val words = line.split(" ")
+
+        when {
+            line.startsWith("$") && words[1] == "cd" -> {
+                current = when (words[2]) {
+                    ".." -> current.parent ?: root
+                    "/" -> root
+                    else -> current.directories.first { it.name == words[2] }
+                }
+            }
+
+            line.startsWith("$") && words[1] == "ls" -> {}
+            words[0] == "dir" -> current.directories.add(Directory(words[1], current))
+            else -> current.files.add(File(words[1], words[0].toInt()))
+        }
+    }
 }
--- 2022/src/test/kotlin/problem/day07/Day07Test.kt
+++ 2022/src/test/kotlin/problem/day07/Day07Test.kt
@@ -6,11 +6,34 @@ import io.kotest.matchers.shouldBe
 class Day07Test : FreeSpec({
 
     val sampleInput: List<String> = listOf(
+        "\$ cd /",
+        "\$ ls",
+        "dir a",
+        "14848514 b.txt",
+        "8504156 c.dat",
+        "dir d",
+        "\$ cd a",
+        "\$ ls",
+        "dir e",
+        "29116 f",
+        "2557 g",
+        "62596 h.lst",
+        "\$ cd e",
+        "\$ ls",
+        "584 i",
+        "\$ cd ..",
+        "\$ cd ..",
+        "\$ cd d",
+        "\$ ls",
+        "4060174 j",
+        "8033020 d.log",
+        "5626152 d.ext",
+        "7214296 k"
     )
 
-    val sampleSolutionPart1: Int = -1
+    val sampleSolutionPart1 = 95437
 
-    val sampleSolutionPart2: Int = -1
+    val sampleSolutionPart2 = 24933642
 
     "Solving day 7" - {
         "part 1 for the sample input should return the correct output" {
