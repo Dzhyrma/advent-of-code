77cf9a6f6c6a8c663b596b09eee2d39305069e4e
Andrii Dzhyrma <andrii.dzhyrma@external.erstegroup.com>
2022-12-09 15:27:16 +0100

    Add 2022 day 9 solution

--- 2022/src/main/kotlin/problem/day09/Day09.kt
+++ 2022/src/main/kotlin/problem/day09/Day09.kt
@@ -3,6 +3,7 @@ package problem.day09
 import common.InputRepo
 import common.readSessionCookie
 import common.solve
+import kotlin.math.absoluteValue
 
 fun main(args: Array<String>) {
     val day = 9
@@ -13,9 +14,42 @@ fun main(args: Array<String>) {
 
 
 fun solveDay09Part1(input: List<String>): Int {
-    TODO()
+    val directions = parseDirections(input)
+    return calculateNumberOfVisistedPointsByTail(directions)
 }
 
 fun solveDay09Part2(input: List<String>): Int {
-    TODO()
+    val directions = parseDirections(input)
+    return calculateNumberOfVisistedPointsByTail(directions, 10)
+}
+
+private fun parseDirections(input: List<String>) = input
+    .map { it.split(" ") }
+    .flatMap { (direction, moves) -> (1..moves.toInt()).map { direction } }
+
+private fun calculateNumberOfVisistedPointsByTail(directions: List<String>, ropeSize: Int = 2): Int {
+    val rope = Array(ropeSize) { Pair(0, 0) }
+    return directions
+        .map { direction ->
+            when (direction) {
+                "U" -> rope.move(0, 0, 1)
+                "D" -> rope.move(0, 0, -1)
+                "L" -> rope.move(0, -1, 0)
+                "R" -> rope.move(0, 1, 0)
+            }
+            rope.last()
+        }
+        .toSet().size
+}
+
+private fun Array<Pair<Int, Int>>.move(index: Int, dx: Int, dy: Int) {
+    this[index] = Pair(this[index].first + dx, this[index].second + dy)
+    if (index == size - 1) return
+    val distanceX = this[index].first - this[index + 1].first
+    val distanceY = this[index].second - this[index + 1].second
+    when {
+        distanceX.absoluteValue == 2 && distanceY.absoluteValue == 2 -> move(index + 1, distanceX / 2, distanceY / 2)
+        distanceX.absoluteValue == 2 -> move(index + 1, distanceX / 2, distanceY)
+        distanceY.absoluteValue == 2 -> move(index + 1, distanceX, distanceY / 2)
+    }
 }
--- 2022/src/test/kotlin/problem/day09/Day09Test.kt
+++ 2022/src/test/kotlin/problem/day09/Day09Test.kt
@@ -6,11 +6,29 @@ import io.kotest.matchers.shouldBe
 class Day09Test : FreeSpec({
 
     val sampleInput: List<String> = listOf(
+        "R 4",
+        "U 4",
+        "L 3",
+        "D 1",
+        "R 4",
+        "D 1",
+        "L 5",
+        "R 2",
+    )
+    val largerInput: List<String> = listOf(
+        "R 5",
+        "U 8",
+        "L 8",
+        "D 3",
+        "R 17",
+        "D 10",
+        "L 25",
+        "U 20",
     )
 
-    val sampleSolutionPart1: Int = -1
+    val sampleSolutionPart1 = 13
 
-    val sampleSolutionPart2: Int = -1
+    val sampleSolutionPart2 = 36
 
     "Solving day 9" - {
         "part 1 for the sample input should return the correct output" {
@@ -18,7 +36,7 @@ class Day09Test : FreeSpec({
         }
 
         "part 2 for the sample input should return the correct output" {
-            solveDay09Part2(sampleInput) shouldBe sampleSolutionPart2
+            solveDay09Part2(largerInput) shouldBe sampleSolutionPart2
         }
     }
 })
