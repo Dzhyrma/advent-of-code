729d54e177cf522d16f2af4f788a21d34254a2d3
Andrii Dzhyrma <andrii.dzhyrma@external.erstegroup.com>
2022-12-10 14:46:45 +0100

    Add 2022 day 10 solution

--- 2022/src/main/kotlin/problem/day10/Day10.kt
+++ 2022/src/main/kotlin/problem/day10/Day10.kt
@@ -11,11 +11,37 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay10Part1, ::solveDay10Part2)
 }
 
+private val IMPORTANT_CYCLES = listOf(20, 60, 100, 140, 180, 220)
 
 fun solveDay10Part1(input: List<String>): Int {
-    TODO()
+    val instructions = input.map { it.split(" ") }
+    val xValues = mutableListOf(1).apply { addSpritePositions(instructions) }
+    return IMPORTANT_CYCLES.sumOf { it * xValues[it - 1] }
 }
 
 fun solveDay10Part2(input: List<String>): Int {
-    TODO()
+    val instructions = input.map { it.split(" ") }
+    val xValues = mutableListOf(1).apply { addSpritePositions(instructions) }
+    (0 until 6).forEach { row ->
+        (0 until 40).forEach { col ->
+            val index = row * 40 + col
+            print(if (col in (xValues[index] - 1..xValues[index] + 1)) '#' else '.')
+        }
+        println()
+    }
+    return -1
+}
+
+private fun MutableList<Int>.addSpritePositions(
+    instructions: List<List<String>>,
+) {
+    instructions.forEach {
+        when (it[0]) {
+            "addx" -> {
+                add(last())
+                add(last() + it[1].toInt())
+            }
+            else -> add(last())
+        }
+    }
 }
