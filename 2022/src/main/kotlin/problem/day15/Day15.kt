53efa08187871cf784e28320a7c38b33e26fdeb8
Andrii Dzhyrma <andrii.dzhyrma@external.erstegroup.com>
2022-12-15 21:20:33 +0100

    Add 2022 day 15 solution

--- 2022/src/main/kotlin/problem/day15/Day15.kt
+++ 2022/src/main/kotlin/problem/day15/Day15.kt
@@ -3,6 +3,9 @@ package problem.day15
 import common.InputRepo
 import common.readSessionCookie
 import common.solve
+import kotlin.math.absoluteValue
+import kotlin.math.max
+import kotlin.math.min
 
 fun main(args: Array<String>) {
     val day = 15
@@ -12,10 +15,74 @@ fun main(args: Array<String>) {
 }
 
 
-fun solveDay15Part1(input: List<String>): Int {
-    TODO()
+fun solveDay15Part1(input: List<String>, rowIndex: Int = 2000000): Int {
+    val sensorsAndBeacons = parseInput(input)
+    val impossibleCoordinates = extractImpossibleRanges(sensorsAndBeacons, rowIndex)
+    return impossibleCoordinates.sumOf { it.last - it.first + 1 }
 }
 
-fun solveDay15Part2(input: List<String>): Int {
-    TODO()
+fun solveDay15Part2(input: List<String>, max: Int = 4000000): Long {
+    val sensorsAndBeacons = parseInput(input)
+    val limitRange = 0 .. max
+    (0 .. max).forEach { row ->
+        val ranges = extractImpossibleRanges(sensorsAndBeacons, row, false)
+            .map { it.intersect(limitRange) }
+            .filter { !it.isEmpty() }
+        when {
+            ranges.isEmpty() -> {}
+            ranges.size > 1 -> return ((ranges.first { it.first == 0 }.last + 1) to row).score()
+            ranges[0].first > 0 -> return (0 to row).score()
+            ranges[0].last < max -> return (max to row).score()
+            else -> {}
+        }
+    }
+    return -1
 }
+
+private fun Pair<Int, Int>.score() = first * 4000000L + second
+
+private fun parseInput(input: List<String>) = input.mapNotNull { INPUT_REGEX.matchEntire(it) }
+    .map { it.groupValues.drop(1).map { value -> value.toInt() } }
+    .map { Pair(it[0], it[1]) to Pair(it[2], it[3]) }
+
+private fun extractImpossibleRanges(
+    sensorsAndBeacons: List<Pair<Pair<Int, Int>, Pair<Int, Int>>>,
+    rowIndex: Int,
+    removeBeacon: Boolean = true,
+): List<IntRange> {
+    val noOtherBeaconRanges = sensorsAndBeacons.flatMap { (sensor, beacon) ->
+        val mDistance = (sensor.first - beacon.first).absoluteValue + (sensor.second - beacon.second).absoluteValue
+        val halfRangeSize = mDistance - (sensor.second - rowIndex).absoluteValue
+        when {
+            halfRangeSize <= 0 -> emptyList()
+            removeBeacon && beacon.second == rowIndex -> listOf(
+                sensor.first - halfRangeSize until beacon.first,
+                beacon.first + 1..sensor.first + halfRangeSize,
+            ).filter { !it.isEmpty() }
+            else -> listOf(sensor.first - halfRangeSize..sensor.first + halfRangeSize)
+        }
+    }
+    return noOtherBeaconRanges.unionAll()
+}
+
+private fun List<IntRange>.unionAll(): List<IntRange> {
+    val ranges = toMutableSet()
+    repeat(size) {
+        val range = ranges.first()
+        ranges.remove(range)
+        val other = ranges.find { other -> other.overlaps(range) }
+        if (other != null) {
+            ranges.remove(other)
+            ranges.add(min(range.first, other.first)..max(range.last, other.last))
+        } else {
+            ranges.add(range)
+        }
+    }
+    return ranges.toList()
+}
+
+private fun IntRange.overlaps(other: IntRange) = first in other || other.first in this
+
+private fun IntRange.intersect(other: IntRange) = max(first, other.first) .. min(last, other.last)
+
+private val INPUT_REGEX = Regex("Sensor at x=(.+), y=(.+): closest beacon is at x=(.+), y=(.+)")
--- 2022/src/test/kotlin/problem/day15/Day15Test.kt
+++ 2022/src/test/kotlin/problem/day15/Day15Test.kt
@@ -6,19 +6,33 @@ import io.kotest.matchers.shouldBe
 class Day15Test : FreeSpec({
 
     val sampleInput: List<String> = listOf(
+        "Sensor at x=2, y=18: closest beacon is at x=-2, y=15",
+        "Sensor at x=9, y=16: closest beacon is at x=10, y=16",
+        "Sensor at x=13, y=2: closest beacon is at x=15, y=3",
+        "Sensor at x=12, y=14: closest beacon is at x=10, y=16",
+        "Sensor at x=10, y=20: closest beacon is at x=10, y=16",
+        "Sensor at x=14, y=17: closest beacon is at x=10, y=16",
+        "Sensor at x=8, y=7: closest beacon is at x=2, y=10",
+        "Sensor at x=2, y=0: closest beacon is at x=2, y=10",
+        "Sensor at x=0, y=11: closest beacon is at x=2, y=10",
+        "Sensor at x=20, y=14: closest beacon is at x=25, y=17",
+        "Sensor at x=17, y=20: closest beacon is at x=21, y=22",
+        "Sensor at x=16, y=7: closest beacon is at x=15, y=3",
+        "Sensor at x=14, y=3: closest beacon is at x=15, y=3",
+        "Sensor at x=20, y=1: closest beacon is at x=15, y=3",
     )
 
-    val sampleSolutionPart1: Int = -1
+    val sampleSolutionPart1 = 26
 
-    val sampleSolutionPart2: Int = -1
+    val sampleSolutionPart2 = 56000011
 
     "Solving day 15" - {
         "part 1 for the sample input should return the correct output" {
-            solveDay15Part1(sampleInput) shouldBe sampleSolutionPart1
+            solveDay15Part1(sampleInput, 10) shouldBe sampleSolutionPart1
         }
 
         "part 2 for the sample input should return the correct output" {
-            solveDay15Part2(sampleInput) shouldBe sampleSolutionPart2
+            solveDay15Part2(sampleInput, 20) shouldBe sampleSolutionPart2
         }
     }
 })
