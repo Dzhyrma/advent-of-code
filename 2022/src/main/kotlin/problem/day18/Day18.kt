d7ebe881d7922fc2c5296f2adaf71d2f5d81449b
Andrii Dzhyrma <andrii.dzhyrma@external.erstegroup.com>
2022-12-18 11:40:13 +0100

    Add 2022 day 18 solution

--- 2022/src/main/kotlin/problem/day18/Day18.kt
+++ 2022/src/main/kotlin/problem/day18/Day18.kt
@@ -11,11 +11,84 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay18Part1, ::solveDay18Part2)
 }
 
+private data class Point(
+    val x: Int,
+    val y: Int,
+    val z: Int,
+) {
+
+    operator fun plus(other: Point) = Point(x + other.x, y + other.y, z + other.z)
+}
 
 fun solveDay18Part1(input: List<String>): Int {
-    TODO()
+    val cubes = parseCubeCoordinates(input)
+    return countSurfaceArea(cubes)
 }
 
 fun solveDay18Part2(input: List<String>): Int {
-    TODO()
+    val cubes = parseCubeCoordinates(input)
+    val xRange = cubes.minOf { it.x }..cubes.maxOf { it.x }
+    val yRange = cubes.minOf { it.y }..cubes.maxOf { it.y }
+    val zRange = cubes.minOf { it.z }..cubes.maxOf { it.z }
+    val knownExteriorCubes = mutableSetOf<Point>()
+    xRange.forEach { x ->
+        yRange.forEach { y ->
+            zRange.forEach { z ->
+                val cube = Point(x, y, z)
+                if (!cubes.contains(cube) && !knownExteriorCubes.contains(cube)) {
+                    val (isInterrior, resultingCubes) = cubes.tryFillInterior(
+                        cube,
+                        xRange,
+                        yRange,
+                        zRange,
+                        knownExteriorCubes
+                    )
+                    if (isInterrior) {
+                        cubes.addAll(resultingCubes)
+                    } else {
+                        knownExteriorCubes.addAll(resultingCubes)
+                    }
+                }
+            }
+        }
+    }
+    return countSurfaceArea(cubes)
 }
+
+private fun countSurfaceArea(cubes: MutableSet<Point>) =
+    cubes.sumOf { cube -> 6 - NEIGHBORS.count { cubes.contains(cube + it) } }
+
+private fun parseCubeCoordinates(input: List<String>) =
+    input.map { it.split(',') }.map { Point(it[0].toInt(), it[1].toInt(), it[2].toInt()) }.toMutableSet()
+
+private fun Set<Point>.tryFillInterior(
+    cube: Point,
+    xRange: IntRange,
+    yRange: IntRange,
+    zRange: IntRange,
+    knownExteriorCubes: Set<Point>,
+    visited: MutableSet<Point> = this.toMutableSet(),
+): Pair<Boolean, Set<Point>> {
+    visited.add(cube)
+    if (cube.x !in xRange || cube.y !in yRange || cube.z !in zRange || knownExteriorCubes.contains(cube))
+        return false to setOf(cube)
+
+    val resultingSet = mutableSetOf(cube)
+    return NEIGHBORS.asSequence()
+        .map { cube + it }
+        .filter { !visited.contains(it) }
+        .map { tryFillInterior(it, xRange, yRange, zRange, knownExteriorCubes, visited) }
+        .all {
+            resultingSet.addAll(it.second)
+            it.first
+        }.let { it to resultingSet }
+}
+
+private val NEIGHBORS = listOf(
+    Point(1, 0, 0),
+    Point(-1, 0, 0),
+    Point(0, 1, 0),
+    Point(0, -1, 0),
+    Point(0, 0, 1),
+    Point(0, 0, -1),
+)
--- 2022/src/test/kotlin/problem/day18/Day18Test.kt
+++ 2022/src/test/kotlin/problem/day18/Day18Test.kt
@@ -5,12 +5,25 @@ import io.kotest.matchers.shouldBe
 
 class Day18Test : FreeSpec({
 
-    val sampleInput: List<String> = listOf(
-    )
+    val sampleInput: List<String> = """
+        2,2,2
+        1,2,2
+        3,2,2
+        2,1,2
+        2,3,2
+        2,2,1
+        2,2,3
+        2,2,4
+        2,2,6
+        1,2,5
+        3,2,5
+        2,1,5
+        2,3,5
+    """.trimIndent().split('\n')
 
-    val sampleSolutionPart1: Int = -1
+    val sampleSolutionPart1 = 64
 
-    val sampleSolutionPart2: Int = -1
+    val sampleSolutionPart2 = 58
 
     "Solving day 18" - {
         "part 1 for the sample input should return the correct output" {
