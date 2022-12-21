ebb7c5d2fcfb9ac9c348f763a24072f7f8fbd1cd
Andrii Dzhyrma <andrii.dzhyrma@external.erstegroup.com>
2022-12-21 11:59:45 +0100

    Add 2022 day 21 solution

--- 2022/src/main/kotlin/problem/day21/Day21.kt
+++ 2022/src/main/kotlin/problem/day21/Day21.kt
@@ -11,11 +11,101 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay21Part1, ::solveDay21Part2)
 }
 
+private data class Operation(
+    val firstMonkey: String,
+    val secondMonkey: String,
+    val operation: Char,
+    var marked: Boolean = false,
+) {
 
-fun solveDay21Part1(input: List<String>): Int {
-    TODO()
+    fun calculate(registry: Map<String, Any>, mark: String? = null): Long {
+        val first = firstMonkey.shoutNumber(registry, mark)
+        val second = secondMonkey.shoutNumber(registry, mark)
+        marked = firstMonkey == mark
+                || secondMonkey == mark
+                || (registry[firstMonkey] as? Operation)?.marked ?: false
+                || (registry[secondMonkey] as? Operation)?.marked ?: false
+
+        return when (operation) {
+            '+' -> first + second
+            '-' -> first - second
+            '*' -> first * second
+            '/' -> first / second
+            else -> throw IllegalArgumentException("Wrong input")
+        }
+    }
+
+    fun findMistake(registry: Map<String, Any>, expectedResult: Long, mistakenMonkey: String): Long {
+        val first = firstMonkey.shoutNumber(registry, mistakenMonkey)
+        val second = secondMonkey.shoutNumber(registry, mistakenMonkey)
+        return when {
+            firstMonkey == mistakenMonkey -> firstMonkeyIsMistaken(expectedResult, second)
+            secondMonkey == mistakenMonkey -> secondMonkeyIsMistaken(expectedResult, first)
+            else -> {
+                val firstOperation = registry[firstMonkey] as? Operation
+                val secondOperation = registry[secondMonkey] as? Operation
+                when {
+                    firstOperation != null && firstOperation.marked -> firstOperation.findMistake(
+                        registry = registry,
+                        expectedResult = firstMonkeyIsMistaken(expectedResult, second),
+                        mistakenMonkey = mistakenMonkey,
+                    )
+                    secondOperation != null && secondOperation.marked -> secondOperation.findMistake(
+                        registry = registry,
+                        expectedResult = secondMonkeyIsMistaken(expectedResult, first),
+                        mistakenMonkey = mistakenMonkey,
+                    )
+                    else -> throw IllegalArgumentException("Wrong input")
+                }
+            }
+        }
+    }
+
+    private fun firstMonkeyIsMistaken(expectedResult: Long, second: Long) = when (operation) {
+        '+' -> expectedResult - second
+        '-' -> expectedResult + second
+        '*' -> expectedResult / second
+        '/' -> expectedResult * second
+        else -> throw IllegalArgumentException("Wrong input")
+    }
+
+    private fun secondMonkeyIsMistaken(expectedResult: Long, first: Long) = when (operation) {
+        '+' -> expectedResult - first
+        '-' -> first - expectedResult
+        '*' -> expectedResult / first
+        '/' -> first / expectedResult
+        else -> throw IllegalArgumentException("Wrong input")
+    }
 }
 
-fun solveDay21Part2(input: List<String>): Int {
-    TODO()
+fun solveDay21Part1(input: List<String>): Long {
+    val registry = parseRegistry(input)
+    return "root".shoutNumber(registry)
 }
+
+fun solveDay21Part2(input: List<String>): Long {
+    val registry = parseRegistry(input)
+    val (firstMonkey, secondMonkey) = (registry["root"] as Operation).let { it.firstMonkey to it.secondMonkey }
+    val first = firstMonkey.shoutNumber(registry, "humn")
+    val second = secondMonkey.shoutNumber(registry, "humn")
+    val (firstOperation, secondOperation) = registry[firstMonkey] as Operation to registry[secondMonkey] as Operation
+    return when {
+        firstOperation.marked -> firstOperation.findMistake(registry, second, "humn")
+        secondOperation.marked -> secondOperation.findMistake(registry, first, "humn")
+        else -> throw IllegalArgumentException("Wrong input")
+    }
+}
+
+private fun parseRegistry(input: List<String>) = input.map { it.split(": ") }.map {
+    it[0] to (it[1].toLongOrNull() ?: REGEX.matchEntire(it[1])?.groupValues?.let { values ->
+        Operation(values[1], values[3], values[2].first())
+    } ?: throw IllegalArgumentException("Wrong input"))
+}.toMap()
+
+private fun String.shoutNumber(registry: Map<String, Any>, mark: String? = null) = when (val outcome = registry[this]) {
+    is Long -> outcome
+    is Operation -> outcome.calculate(registry, mark)
+    else -> throw IllegalArgumentException("Wrong input")
+}
+
+private val REGEX = Regex("(.+) ([+\\-*/]) (.+)")
--- 2022/src/test/kotlin/problem/day21/Day21Test.kt
+++ 2022/src/test/kotlin/problem/day21/Day21Test.kt
@@ -5,12 +5,27 @@ import io.kotest.matchers.shouldBe
 
 class Day21Test : FreeSpec({
 
-    val sampleInput: List<String> = listOf(
-    )
+    val sampleInput: List<String> = """
+        root: pppw + sjmn
+        dbpl: 5
+        cczh: sllz + lgvd
+        zczc: 2
+        ptdq: humn - dvpt
+        dvpt: 3
+        lfqf: 4
+        humn: 5
+        ljgn: 2
+        sjmn: drzm * dbpl
+        sllz: 4
+        pppw: cczh / lfqf
+        lgvd: ljgn * ptdq
+        drzm: hmdt - zczc
+        hmdt: 32
+    """.trimIndent().split('\n')
 
-    val sampleSolutionPart1: Int = -1
+    val sampleSolutionPart1 = 152
 
-    val sampleSolutionPart2: Int = -1
+    val sampleSolutionPart2 = 301
 
     "Solving day 21" - {
         "part 1 for the sample input should return the correct output" {
