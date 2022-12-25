e58fe44956302f0735dae317a842e1d4bccc564f
Andrii Dzhyrma <andrii.dzhyrma@external.erstegroup.com>
2022-12-25 12:17:03 +0100

    Add 2022 day 25 solution

--- 2022/src/main/kotlin/problem/day25/Day25.kt
+++ 2022/src/main/kotlin/problem/day25/Day25.kt
@@ -3,6 +3,7 @@ package problem.day25
 import common.InputRepo
 import common.readSessionCookie
 import common.solve
+import java.math.BigInteger
 
 fun main(args: Array<String>) {
     val day = 25
@@ -12,10 +13,37 @@ fun main(args: Array<String>) {
 }
 
 
-fun solveDay25Part1(input: List<String>): Int {
-    TODO()
+fun solveDay25Part1(input: List<String>): String {
+    return input.sumOf { it.toLongFromSnafu() }.toSnafu()
 }
 
-fun solveDay25Part2(input: List<String>): Int {
-    TODO()
+fun solveDay25Part2(input: List<String>): String {
+    TODO("There is no second part")
 }
+
+private fun String.toLongFromSnafu(): Long {
+    return reversed().foldIndexed(0L) { index, acc, char -> acc + POW_OF_5S[index] * SNAFU_MAP.getValue(char) }
+}
+
+private fun Long.toSnafu(): String {
+    var current = this
+    return POW_OF_5S.indices.reversed()
+        .map { index ->
+            val pow = POW_OF_5S[index]
+            val halfPow = pow / 2
+            when {
+                current > pow * 2 + halfPow
+                        || current < -pow * 2 - halfPow -> throw IllegalArgumentException("Number is too big")
+                current in -halfPow..halfPow -> '0'
+                current > pow + halfPow -> '2'
+                current > halfPow -> '1'
+                current < -pow - halfPow -> '='
+                else -> '-'
+            }.also { current -= POW_OF_5S[index] * SNAFU_MAP.getValue(it) }
+        }
+        .dropWhile { it == '0' }
+        .let { String(it.toCharArray()) }
+}
+
+private val SNAFU_MAP = mapOf<Char, Long>('2' to 2, '1' to 1, '0' to 0, '-' to -1, '=' to -2)
+private val POW_OF_5S = (0 until 27).map { BigInteger.valueOf(5).pow(it).toLong() }
--- 2022/src/test/kotlin/problem/day25/Day25Test.kt
+++ 2022/src/test/kotlin/problem/day25/Day25Test.kt
@@ -5,20 +5,27 @@ import io.kotest.matchers.shouldBe
 
 class Day25Test : FreeSpec({
 
-    val sampleInput: List<String> = listOf(
-    )
+    val sampleInput: List<String> = """
+        1=-0-2
+        12111
+        2=0=
+        21
+        2=01
+        111
+        20012
+        112
+        1=-1=
+        1-12
+        12
+        1=
+        122
+    """.trimIndent().split('\n')
 
-    val sampleSolutionPart1: Int = -1
-
-    val sampleSolutionPart2: Int = -1
+    val sampleSolutionPart1 = "2=-1=0"
 
     "Solving day 25" - {
         "part 1 for the sample input should return the correct output" {
             solveDay25Part1(sampleInput) shouldBe sampleSolutionPart1
-        }
-
-        "part 2 for the sample input should return the correct output" {
-            solveDay25Part2(sampleInput) shouldBe sampleSolutionPart2
         }
     }
 })
