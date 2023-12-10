08c8794d6886e8391a584b8cd908d83935ce4eb4
Andrii Dzhyrma <andrii.dzhyrma@external.erstegroup.com>
2023-12-10 14:52:55 +0100

    Add 2023 day 10 solution

--- 2023/src/main/kotlin/problem/day10/Day10.kt
+++ 2023/src/main/kotlin/problem/day10/Day10.kt
@@ -12,9 +12,121 @@ fun main(args: Array<String>) {
 }
 
 fun solveDay10Part1(input: List<String>): Long {
-    TODO()
+    val distances = Array(input.size) { IntArray(input[0].length) { Int.MIN_VALUE } }
+    solvePart1(input, distances)
+    return distances.maxOf { it.max() }.toLong()
 }
 
 fun solveDay10Part2(input: List<String>): Long {
-    TODO()
+    val distances = Array(input.size) { IntArray(input[0].length) { Int.MIN_VALUE } }
+    solvePart1(input, distances)
+    val fixedInput = input.replaceStartingSymbol(findStartingPoint(input))
+    return distances.mapIndexed { col, column ->
+        var isInLoop = false
+        var curvedPipe: Char? = null
+        column.mapIndexed { row, i ->
+            if (i != Int.MIN_VALUE) {
+                when {
+                    fixedInput[col][row] == '|' -> isInLoop = !isInLoop
+                    curvedPipe == null && fixedInput[col][row] != '-' -> curvedPipe = fixedInput[col][row]
+                    (curvedPipe == 'L' && fixedInput[col][row] == '7')
+                            || (curvedPipe == 'F' && fixedInput[col][row] == 'J') -> {
+                        isInLoop = !isInLoop
+                        curvedPipe = null
+                    }
+                    curvedPipe != null && fixedInput[col][row] != '-' -> curvedPipe = null
+                }
+                0
+            } else if (isInLoop) 1 else 0
+        }.sum()
+    }.sum().toLong()
 }
+
+private fun List<String>.replaceStartingSymbol(startPoint: Pair<Int, Int>): List<String> {
+    val connectedTop = startPoint.first - 1 in indices
+            && (this[startPoint.first - 1][startPoint.second] == '|'
+            || this[startPoint.first - 1][startPoint.second] == 'F'
+            || this[startPoint.first - 1][startPoint.second] == '7')
+    val connectedBottom = startPoint.first + 1 in indices
+            && (this[startPoint.first + 1][startPoint.second] == '|'
+            || this[startPoint.first + 1][startPoint.second] == 'J'
+            || this[startPoint.first + 1][startPoint.second] == 'L')
+    val connectedLeft = startPoint.second - 1 in this[0].indices
+            && (this[startPoint.first][startPoint.second - 1] == '-'
+            || this[startPoint.first][startPoint.second - 1] == 'F'
+            || this[startPoint.first][startPoint.second - 1] == 'L')
+    val connectedRight = startPoint.second + 1 in this[0].indices
+            && (this[startPoint.first][startPoint.second + 1] == '-'
+            || this[startPoint.first][startPoint.second + 1] == 'J'
+            || this[startPoint.first][startPoint.second + 1] == '7')
+    val actualSymbol = when {
+        connectedLeft && connectedTop -> 'J'
+        connectedLeft && connectedRight -> '-'
+        connectedLeft && connectedBottom -> '7'
+        connectedTop && connectedRight -> 'L'
+        connectedTop && connectedBottom -> '|'
+        connectedRight && connectedBottom -> 'F'
+        else -> error("Wrong input")
+    }
+    return mapIndexed { col, line -> if (col == startPoint.first) line.replace('S', actualSymbol) else line }
+}
+
+private fun solvePart1(input: List<String>, distances: Array<IntArray>) {
+    val startPoint = findStartingPoint(input)
+    distances[startPoint.first][startPoint.second] = 0
+    fillDistances(startPoint, input, distances)
+}
+
+private fun findStartingPoint(input: List<String>) =
+    input.mapIndexed { col, line -> col to line.indexOf('S') }.first { it.second >= 0 }
+
+private fun fillDistances(
+    startPoint: Pair<Int, Int>,
+    input: List<String>,
+    distances: Array<IntArray>,
+    predicate: (Pair<Int, Int>, Pair<Int, Int>, Int) -> Boolean = { prevCoord, nextCoord, i ->
+        distances[nextCoord.first][nextCoord.second] == Int.MIN_VALUE
+                && CONNECTED[i].contains(input[nextCoord.first][nextCoord.second])
+                && (input[prevCoord.first][prevCoord.second] == 'S'
+                || CONNECTED[(i + 2) % 4].contains(input[prevCoord.first][prevCoord.second]))
+    }
+) {
+    var points = listOf(startPoint)
+    while (points.isNotEmpty()) {
+        val nextPoints = mutableListOf<Pair<Int, Int>>()
+        points.forEach { point ->
+            (0..3).forEach { i ->
+                val coord = (point.first + DY[i]) to (point.second + DX[i])
+                if (coord.first in distances.indices
+                    && coord.second in distances[0].indices
+                    && predicate(point, coord, i)
+                ) {
+                    distances[coord.first][coord.second] = distances[point.first][point.second] + 1
+                    nextPoints.add(coord)
+                }
+            }
+        }
+        points = nextPoints
+    }
+}
+
+private fun List<String>.printLabyrinth(distances: Array<IntArray>? = null) = forEachIndexed { col, line ->
+    line
+        .replace('F', '╔')
+        .replace('J', '╝')
+        .replace('L', '╚')
+        .replace('7', '╗')
+        .replace('-', '═')
+        .replace('|', '║')
+        .mapIndexed { row, char -> if (distances != null && distances[col][row] != Int.MIN_VALUE) '#' else char }
+        .also { println(String(it.toCharArray())) }
+}
+
+private val DX = intArrayOf(-1, 0, 1, 0)
+private val DY = intArrayOf(0, 1, 0, -1)
+private val CONNECTED = listOf(
+    setOf('F', 'L', '-'),
+    setOf('J', 'L', '|'),
+    setOf('J', '7', '-'),
+    setOf('F', '7', '|'),
+)
--- 2023/src/test/kotlin/problem/day10/Day10Test.kt
+++ 2023/src/test/kotlin/problem/day10/Day10Test.kt
@@ -5,20 +5,75 @@ import io.kotest.matchers.shouldBe
 
 class Day10Test : FreeSpec({
 
-    val sampleInput: List<String> = listOf(
+    val sampleInput1: List<String> = listOf(
+        ".....",
+        ".S-7.",
+        ".|.|.",
+        ".L-J.",
+        ".....",
+    )
+    val sampleInput2: List<String> = listOf(
+        "..F7.",
+        ".FJ|.",
+        "SJ.L7",
+        "|F--J",
+        "LJ...",
+    )
+    val sampleInput3: List<String> = listOf(
+        "..........",
+        ".S------7.",
+        ".|F----7|.",
+        ".||....||.",
+        ".||....||.",
+        ".|L-7F-J|.",
+        ".|..||..|.",
+        ".L--JL--J.",
+        "..........",
+    )
+    val sampleInput4: List<String> = listOf(
+        ".F----7F7F7F7F-7....",
+        ".|F--7||||||||FJ....",
+        ".||.FJ||||||||L7....",
+        "FJL7L7LJLJ||LJ.L-7..",
+        "L--J.L7...LJS7F-7L7.",
+        "....F-J..F7FJ|L7L7L7",
+        "....L7.F7||L7|.L7L7|",
+        ".....|FJLJ|FJ|F7|.LJ",
+        "....FJL-7.||.||||...",
+        "....L---J.LJ.LJLJ...",
+    )
+    val sampleInput5: List<String> = listOf(
+        "FF7FSF7F7F7F7F7F---7",
+        "L|LJ||||||||||||F--J",
+        "FL-7LJLJ||||||LJL-77",
+        "F--JF--7||LJLJ7F7FJ-",
+        "L---JF-JLJ.||-FJLJJ7",
+        "|F|F-JF---7F7-L7L|7|",
+        "|FFJF7L7F-JF7|JL---7",
+        "7-L-JL7||F7|L7F-7F7|",
+        "L.L7LFJ|||||FJL7||LJ",
+        "L7JLJL-JLJLJL--JLJ.L",
     )
 
-    val sampleSolutionPart1 = -1L
+    val sample1SolutionPart1 = 4
+    val sample2SolutionPart1 = 8
 
-    val sampleSolutionPart2 = -1L
+    val sample1SolutionPart2 = 1
+    val sample3SolutionPart2 = 4
+    val sample4SolutionPart2 = 8
+    val sample5SolutionPart2 = 10
 
     "Solving day 10" - {
         "part 1 for the sample input should return the correct output" {
-            solveDay10Part1(sampleInput) shouldBe sampleSolutionPart1
+            solveDay10Part1(sampleInput1) shouldBe sample1SolutionPart1
+            solveDay10Part1(sampleInput2) shouldBe sample2SolutionPart1
         }
 
         "part 2 for the sample input should return the correct output" {
-            solveDay10Part2(sampleInput) shouldBe sampleSolutionPart2
+            solveDay10Part2(sampleInput1) shouldBe sample1SolutionPart2
+            solveDay10Part2(sampleInput3) shouldBe sample3SolutionPart2
+            solveDay10Part2(sampleInput4) shouldBe sample4SolutionPart2
+            solveDay10Part2(sampleInput5) shouldBe sample5SolutionPart2
         }
     }
 })
