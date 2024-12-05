d7f9d3985def8d4afde26b5bdd38ee0c38fd0edd
Andrii Dzhyrma <sr03a@s-mxs.net>
2024-12-05 06:59:58 +0100

    Add 2024 day 5 solution

--- 2024/src/main/kotlin/problem/day05/Day05.kt
+++ 2024/src/main/kotlin/problem/day05/Day05.kt
@@ -11,10 +11,40 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay05Part1, ::solveDay05Part2)
 }
 
-fun solveDay05Part1(input: List<String>): Long {
-    return TODO()
+fun solveDay05Part1(input: List<String>): Int {
+    val (rules, updates) = parseInput(input)
+    return updates.filter { it.isOrdered(rules) }.sumOf { it[it.size / 2] }
 }
 
-fun solveDay05Part2(input: List<String>): Long {
-    return TODO()
+fun solveDay05Part2(input: List<String>): Int {
+    val (rules, updates) = parseInput(input)
+    return updates.filter { !it.isOrdered(rules) }.map { it.reorder(rules) }.sumOf { it[it.size / 2] }
+}
+
+private fun parseInput(input: List<String>): Pair<Map<Int, Set<Int>>, List<List<Int>>> {
+    val rules = input.takeWhile { it != "" }
+        .map { it.split("|").map(String::toInt) }
+        .groupBy({ it[0] }) { it[1] }
+        .mapValues { (_, pageList) -> pageList.toSet() }
+    val updates = input.takeLastWhile { it != "" }.map { it.split(',').map(String::toInt) }
+    return rules to updates
+}
+
+private fun List<Int>.isOrdered(rules: Map<Int, Set<Int>>): Boolean {
+    val positions = withIndex().associate { it.value to it.index }
+    return rules.none { (pageBefore, pagesAfter) ->
+        positions.contains(pageBefore) && pagesAfter.any { pageAfter ->
+            positions.contains(pageAfter) && positions.getValue(pageBefore) > positions.getValue(pageAfter)
+        }
+    }
+}
+
+private fun List<Int>.reorder(rules: Map<Int, Set<Int>>): List<Int> {
+    return sortedWith { o1, o2 ->
+        when {
+            rules.containsKey(o1) && rules.getValue(o1).contains(o2) -> -1
+            rules.containsKey(o2) && rules.getValue(o2).contains(o1) -> 1
+            else -> o1.compareTo(o2)
+        }
+    }
 }
--- 2024/src/test/kotlin/problem/day05/Day05Test.kt
+++ 2024/src/test/kotlin/problem/day05/Day05Test.kt
@@ -6,22 +6,47 @@ import io.kotest.matchers.shouldBe
 class Day05Test : FreeSpec({
 
     val sampleInput1: List<String> = listOf(
+        "47|53",
+        "97|13",
+        "97|61",
+        "97|47",
+        "75|29",
+        "61|13",
+        "75|53",
+        "29|13",
+        "97|29",
+        "53|29",
+        "61|53",
+        "97|53",
+        "61|29",
+        "47|13",
+        "75|47",
+        "97|75",
+        "47|61",
+        "75|61",
+        "47|29",
+        "75|13",
+        "53|13",
+        "",
+        "75,47,61,53,29",
+        "97,61,53,29,13",
+        "75,29,13",
+        "75,97,47,61,53",
+        "61,13,29",
+        "97,13,75,29,47",
     )
 
-    val sampleInput2: List<String> = listOf(
-    )
+    val sampleSolutionPart1 = 143
 
-    val sampleSolutionPart1 = 18
+    val sampleSolutionPart2 = 123
 
-    val sampleSolutionPart2 = 9
-
-    "Solving day 1" - {
+    "Solving day 5" - {
         "part 1 for the sample input should return the correct output" {
             solveDay05Part1(sampleInput1) shouldBe sampleSolutionPart1
         }
 
         "part 2 for the sample input should return the correct output" {
-            solveDay05Part2(sampleInput2) shouldBe sampleSolutionPart2
+            solveDay05Part2(sampleInput1) shouldBe sampleSolutionPart2
         }
     }
 })
