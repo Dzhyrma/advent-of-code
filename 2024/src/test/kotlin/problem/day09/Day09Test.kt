b20729afa54b7bfb72c528a56ef6c68d1d434292
Andrii Dzhyrma <sr03a@s-mxs.net>
2024-12-07 06:24:50 +0100

    Add 2024 day 9 template

---
+++ 2024/src/test/kotlin/problem/day09/Day09Test.kt
@@ -0,0 +1,25 @@
+package problem.day09
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+class Day09Test : FreeSpec({
+
+    val sampleInput1: List<String> = listOf()
+
+    val sampleInput2: List<String> = listOf()
+
+    val sampleSolutionPart1 = -1
+
+    val sampleSolutionPart2 = -1
+
+    "Solving day 9" - {
+        "part 1 for the sample input should return the correct output" {
+            solveDay09Part1(sampleInput1) shouldBe sampleSolutionPart1
+        }
+
+        "part 2 for the sample input should return the correct output" {
+            solveDay09Part2(sampleInput2) shouldBe sampleSolutionPart2
+        }
+    }
+})
