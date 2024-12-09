7774eaf0c90c30300fa1eaa7237c9a7c3b7e307c
Andrii Dzhyrma <sr03a@s-mxs.net>
2024-12-09 21:25:57 +0100

    Add 2024 day 9 refactorings

--- 2024/src/main/kotlin/problem/day09/Day09.kt
+++ 2024/src/main/kotlin/problem/day09/Day09.kt
@@ -17,7 +17,7 @@ fun main(args: Array<String>) {
 
 fun solveDay09Part1(input: List<String>): Long {
     val memoryHash = input[0].map { it.digitToInt() }.toIntArray()
-    var memoryIndex = 0L
+    var currentMemoryBlockIndex = 0L
     var memoryLastFileIndex = ((memoryHash.size - 1) shr 1) shl 1
     var checksum = 0L
     memoryHash.forEachIndexed { index, length ->
@@ -25,18 +25,18 @@ fun solveDay09Part1(input: List<String>): Long {
             memoryLastFileIndex < index -> return@forEachIndexed // everything already moved
             // file
             index % 2 == 0 -> {
-                checksum += fileHash(index shr 1, length, memoryIndex)
-                memoryIndex += length
+                checksum += fileChecksum(index shr 1, length, currentMemoryBlockIndex)
+                currentMemoryBlockIndex += length
             }
             // empty memory space
             else -> {
                 var emptyMemoryLength = length
                 while (emptyMemoryLength > 0 && memoryLastFileIndex >= index) {
                     val fileChunkLength = min(memoryHash[memoryLastFileIndex], emptyMemoryLength)
-                    checksum += fileHash(memoryLastFileIndex shr 1, fileChunkLength, memoryIndex)
+                    checksum += fileChecksum(memoryLastFileIndex shr 1, fileChunkLength, currentMemoryBlockIndex)
                     memoryHash[memoryLastFileIndex] -= fileChunkLength
                     emptyMemoryLength -= fileChunkLength
-                    memoryIndex += fileChunkLength
+                    currentMemoryBlockIndex += fileChunkLength
                     if (memoryHash[memoryLastFileIndex] == 0) memoryLastFileIndex -= 2
                 }
             }
@@ -51,7 +51,7 @@ fun solveDay09Part2(input: List<String>): Long {
     defrag(files, emptyBlocks)
 
     return files.mapIndexed { fileId, (fileBlockIndex, fileLength) ->
-        fileHash(fileId, fileLength, fileBlockIndex.toLong())
+        fileChecksum(fileId, fileLength, fileBlockIndex.toLong())
     }.sum()
 }
 
@@ -87,7 +87,7 @@ private fun defrag(files: Array<FileInfo>, emptyBlocks: TreeMap<Int, Int>) {
     }
 }
 
-private fun fileHash(fileId: Int, fileLength: Int, memoryBlockIndex: Long) =
+private fun fileChecksum(fileId: Int, fileLength: Int, memoryBlockIndex: Long) =
     (fileId * ((memoryBlockIndex shl 1) + fileLength - 1) * fileLength) shr 1
 
 private val DUMMY_FILE = FileInfo(0, 0)
