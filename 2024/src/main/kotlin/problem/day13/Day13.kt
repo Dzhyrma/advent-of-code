a5210e9f67cc0aa0454352a81ccd297c824fcec5
Andrii Dzhyrma <sr03a@s-mxs.net>
2024-12-13 07:55:39 +0100

    Add 2024 day 13 solution

--- 2024/src/main/kotlin/problem/day13/Day13.kt
+++ 2024/src/main/kotlin/problem/day13/Day13.kt
@@ -12,9 +12,28 @@ fun main(args: Array<String>) {
 }
 
 fun solveDay13Part1(input: List<String>): Long {
-    return TODO()
+    return input.calculateMinimumTokens()
 }
 
 fun solveDay13Part2(input: List<String>): Long {
-    return TODO()
+    return input.calculateMinimumTokens(adjust = true)
+}
+
+private fun List<String>.calculateMinimumTokens(adjust: Boolean = false): Long =
+    (indices step 4).sumOf { i ->
+        val buttonA = this[i].split(": ")[1].split(", ").map { it.substring(2).toInt() }
+        val buttonB = this[i + 1].split(": ")[1].split(", ").map { it.substring(2).toInt() }
+        val prize = this[i + 2].split(": ")[1].split(", ")
+            .map { it.substring(2).toLong() + if (adjust) 10_000_000_000_000L else 0 }
+
+        findSolution(buttonA[0], buttonA[1], buttonB[0], buttonB[1], prize[0], prize[1])
+    }
+
+private fun findSolution(dxA: Int, dyA: Int, dxB: Int, dyB: Int, prizeX: Long, prizeY: Long): Long {
+    val a = (dxB * prizeY - dyB * prizeX) / (dyA * dxB - dxA * dyB)
+    val b = (prizeX - a * dxA) / dxB
+    if ((prizeX - a * dxA) % dxB == 0L && (dxB * prizeY - dyB * prizeX) % (dyA * dxB - dxA * dyB) == 0L) {
+        return 3 * a + b
+    }
+    return 0
 }
--- 2024/src/test/kotlin/problem/day13/Day13Test.kt
+++ 2024/src/test/kotlin/problem/day13/Day13Test.kt
@@ -5,11 +5,27 @@ import io.kotest.matchers.shouldBe
 
 class Day13Test : FreeSpec({
 
-    val sampleInput1: List<String> = listOf()
+    val sampleInput1: List<String> = listOf(
+        "Button A: X+94, Y+34",
+        "Button B: X+22, Y+67",
+        "Prize: X=8400, Y=5400",
+        "",
+        "Button A: X+26, Y+66",
+        "Button B: X+67, Y+21",
+        "Prize: X=12748, Y=12176",
+        "",
+        "Button A: X+17, Y+86",
+        "Button B: X+84, Y+37",
+        "Prize: X=7870, Y=6450",
+        "",
+        "Button A: X+69, Y+23",
+        "Button B: X+27, Y+71",
+        "Prize: X=18641, Y=10279",
+    )
 
-    val sampleSolutionPart1 = -1
+    val sampleSolutionPart1 = 480
 
-    val sampleSolutionPart2 = -1
+    val sampleSolutionPart2 = 875318608908L
 
     "Solving day 13" - {
         "part 1 for the sample input should return the correct output" {
