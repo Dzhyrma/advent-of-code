b891947433875045fec3380ec088de957fbba8f8
Andrii Dzhyrma <sr03a@s-mxs.net>
2024-12-15 08:18:55 +0100

    Add 2024 day 15 solution

--- 2024/src/main/kotlin/problem/day15/Day15.kt
+++ 2024/src/main/kotlin/problem/day15/Day15.kt
@@ -12,9 +12,155 @@ fun main(args: Array<String>) {
 }
 
 fun solveDay15Part1(input: List<String>): Long {
-    return TODO()
+    return calculateGPSCoordinates(input)
 }
 
 fun solveDay15Part2(input: List<String>): Long {
-    return TODO()
+    return calculateGPSCoordinates2(input)
+}
+
+private fun calculateGPSCoordinates(input: List<String>): Long {
+    val map = input.takeWhile { it.contains('#') }.map { it.toCharArray() }
+    val moves = input.dropWhile { it.contains('#') }.joinToString("").toCharArray()
+    val robot = map.withIndex().flatMap { (y, row) -> row.withIndex().map { (x, c) -> Triple(x, y, c) } }
+        .first { it.third == '@' }.let { Pair(it.first, it.second) }
+    val boxes = map.withIndex().flatMap { (y, row) ->
+        row.withIndex().mapNotNull { (x, c) -> if (c == 'O') x to y else null }
+    }.toMutableSet()
+
+    val directions = mapOf('<' to (-1 to 0), '>' to (1 to 0), '^' to (0 to -1), 'v' to (0 to 1))
+    var rx = robot.first
+    var ry = robot.second
+
+    fun firstEmptySpaceAfterBoxes(x: Int, y: Int, dx: Int, dy: Int): Pair<Int, Int>? {
+        var nx = x
+        var ny = y
+        while (nx to ny in boxes) {
+            nx += dx
+            ny += dy
+            if (map[ny][nx] == '#') return null
+        }
+        return nx to ny
+    }
+
+    for (move in moves) {
+        val (dx, dy) = directions[move]!!
+        val (nx, ny) = rx + dx to ry + dy
+        if (map[ny][nx] == '#') continue
+        if (nx to ny in boxes) {
+            val e = firstEmptySpaceAfterBoxes(nx, ny, dx, dy)
+            if (e != null) {
+                val (cx, cy) = e
+                boxes.add(cx to cy)
+                boxes.remove(nx to ny)
+                rx = nx
+                ry = ny
+            }
+        } else {
+            rx = nx
+            ry = ny
+        }
+    }
+    map.forEachIndexed { y, chars ->
+        chars.forEachIndexed { x, c ->
+            if (x to y in boxes) {
+                print('O')
+            } else if (x to y == rx to ry) {
+                print('@')
+            } else if (c == '#') {
+                print(c)
+            } else {
+                print('.')
+            }
+        }
+        println()
+    }
+    return boxes.sumOf { (x, y) -> (y * 100 + x) }.toLong()
+}
+
+private fun calculateGPSCoordinates2(input: List<String>): Long {
+    val originalMap = input.takeWhile { it.contains('#') }
+    val moves = input.dropWhile { it.contains('#') }.joinToString("")
+    val newMap = originalMap.map { row ->
+        row.flatMap { c ->
+            when (c) {
+                '#' -> "##".toList()
+                'O' -> "[]".toList()
+                '.' -> "..".toList()
+                '@' -> "@.".toList()
+                else -> emptyList()
+            }
+        }.joinToString("").toCharArray()
+    }
+
+    val robot = newMap.withIndex().flatMap { (y, row) ->
+        row.withIndex().mapNotNull { (x, c) -> if (c == '@') x to y else null }
+    }.first()
+    val boxes = newMap.withIndex().flatMap { (y, row) ->
+        row.withIndex().mapNotNull { (x, c) -> if (row.getOrNull(x + 1) == ']' && c == '[') x to y else null }
+    }.toMutableSet()
+
+    val directions = mapOf('<' to (-1 to 0), '>' to (1 to 0), '^' to (0 to -1), 'v' to (0 to 1))
+    var rx = robot.first
+    var ry = robot.second
+
+    fun canPushBoxes(x: Int, y: Int, dx: Int, dy: Int): Boolean {
+        return when {
+            newMap[y + dy][x + dx] == '#' || newMap[y + dy][x + dx + 1] == '#' -> false
+            (dy == 0) && x + 2 * dx to y in boxes -> canPushBoxes(x + 2 * dx, y, dx, 0)
+            (dy == 0) -> true
+            x to y + dy in boxes -> canPushBoxes(x, y + dy, 0, dy)
+            x - 1 to y + dy in boxes && x + 1 to y + dy in boxes -> canPushBoxes(x - 1, y + dy, 0, dy) &&
+                canPushBoxes(x + 1, y + dy, 0, dy)
+            x - 1 to y + dy in boxes -> canPushBoxes(x - 1, y + dy, 0, dy)
+            x + 1 to y + dy in boxes -> canPushBoxes(x + 1, y + dy, 0, dy)
+            else -> true
+        }
+    }
+
+    fun pushBoxes(x: Int, y: Int, dx: Int, dy: Int) {
+        if ((dy == 0) && x + 2 * dx to y in boxes) pushBoxes(x + 2 * dx, y, dx, 0)
+        if (dy != 0) {
+            if (x to y + dy in boxes) pushBoxes(x, y + dy, 0, dy)
+            if (x - 1 to y + dy in boxes) pushBoxes(x - 1, y + dy, 0, dy)
+            if (x + 1 to y + dy in boxes) pushBoxes(x + 1, y + dy, 0, dy)
+        }
+        boxes.remove(x to y)
+        boxes.add(x + dx to y + dy)
+    }
+
+    for (move in moves) {
+        val (dx, dy) = directions[move]!!
+        val (nx, ny) = rx + dx to ry + dy
+        if (newMap[ny][nx] == '#') continue
+        when {
+            (nx to ny in boxes) && canPushBoxes(nx, ny, dx, dy) -> pushBoxes(nx, ny, dx, dy)
+            dx == -1 && (nx - 1 to ny in boxes) && canPushBoxes(nx - 1, ny, -1, 0) -> pushBoxes(nx - 1, ny, -1, 0)
+            (dy != 0) && (nx - 1) to ny in boxes && canPushBoxes(nx - 1, ny, 0, dy) -> pushBoxes(nx - 1, ny, 0, dy)
+        }
+        if (nx to ny !in boxes && nx - 1 to ny !in boxes) {
+            rx = nx
+            ry = ny
+        }
+    }
+
+    var sum = 0
+    newMap.forEachIndexed { y, chars ->
+        chars.forEachIndexed { x, c ->
+            if (x to y == rx to ry) {
+                print('@')
+            } else if (x to y in boxes) {
+                sum += y * 100 + x
+                print('[')
+            } else if (x - 1 to y in boxes) {
+                print(']')
+            } else if (c == '#') {
+                print('#')
+            } else {
+                print('.')
+            }
+        }
+        println()
+    }
+    return sum.toLong()
 }
--- 2024/src/test/kotlin/problem/day15/Day15Test.kt
+++ 2024/src/test/kotlin/problem/day15/Day15Test.kt
@@ -5,19 +5,68 @@ import io.kotest.matchers.shouldBe
 
 class Day15Test : FreeSpec({
 
-    val sampleInput1: List<String> = listOf()
+    val sampleInput1: List<String> = listOf(
+        "########",
+        "#..O.O.#",
+        "##@.O..#",
+        "#...O..#",
+        "#.#.O..#",
+        "#...O..#",
+        "#......#",
+        "########",
+        "",
+        "<^^>>>vv<v>>v<<",
+    )
+    val sampleInput2: List<String> = listOf(
+        "##########",
+        "#..O..O.O#",
+        "#......O.#",
+        "#.OO..O.O#",
+        "#..O@..O.#",
+        "#O#..O...#",
+        "#O..O..O.#",
+        "#.OO.O.OO#",
+        "#....O...#",
+        "##########",
+        "",
+        "<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^",
+        "vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v",
+        "><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<",
+        "<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^",
+        "^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><",
+        "^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^",
+        ">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^",
+        "<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>",
+        "^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>",
+        "v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
+    )
+    val sampleInput3: List<String> = listOf(
+        "#######",
+        "#...#.#",
+        "#.....#",
+        "#..OO@#",
+        "#..O..#",
+        "#.....#",
+        "#######",
+        "",
+        "<vv<<^^<<^^",
+    )
 
-    val sampleSolutionPart1 = -1L
+    val sampleSolutionPart1_1 = 2028L
+    val sampleSolutionPart1_2 = 10092L
 
-    val sampleSolutionPart2 = -1L
+    val sampleSolutionPart2_1 = 618L
+    val sampleSolutionPart2_2 = 9021L
 
     "Solving day 15" - {
         "part 1 for the sample input should return the correct output" {
-            solveDay15Part1(sampleInput1) shouldBe sampleSolutionPart1
+            solveDay15Part1(sampleInput1) shouldBe sampleSolutionPart1_1
+            solveDay15Part1(sampleInput2) shouldBe sampleSolutionPart1_2
         }
 
         "part 2 for the sample input should return the correct output" {
-            solveDay15Part2(sampleInput1) shouldBe sampleSolutionPart2
+            solveDay15Part2(sampleInput3) shouldBe sampleSolutionPart2_1
+            solveDay15Part2(sampleInput2) shouldBe sampleSolutionPart2_2
         }
     }
 })
