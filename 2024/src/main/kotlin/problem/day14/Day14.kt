0e3bfa4f76d34262080b770abfbd6db6c74b97d2
Andrii Dzhyrma <sr03a@s-mxs.net>
2024-12-15 05:47:11 +0100

    Add 2024 day 14 solution

--- 2024/src/main/kotlin/problem/day14/Day14.kt
+++ 2024/src/main/kotlin/problem/day14/Day14.kt
@@ -11,10 +11,70 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay14Part1, ::solveDay14Part2)
 }
 
-fun solveDay14Part1(input: List<String>): Long {
-    return TODO()
+fun solveDay14Part1(input: List<String>, width: Int = 101, height: Int = 103): Int {
+    return input.parseRobots().calculateSafetyFactor(100, width, height)
 }
 
-fun solveDay14Part2(input: List<String>): Long {
-    return TODO()
+fun solveDay14Part2(input: List<String>): Int {
+    var min = Int.MAX_VALUE
+    var minI = -1L
+    repeat(10000) { i ->
+        val res = input.parseRobots().calculateSafetyFactor(i.toLong())
+        if (res < min) {
+            min = res
+            minI = i.toLong()
+        }
+    }
+    println(minI)
+    input.parseRobots().calculateSafetyFactor(minI, log = true)
+    return min
 }
+
+private fun List<String>.parseRobots() = map { line ->
+    val (px, py, vx, vy) = Regex("p=(-?\\d+),(-?\\d+) v=(-?\\d+),(-?\\d+)")
+        .find(line)!!
+        .destructured
+        .toList()
+        .map { it.toInt() }
+    Robot(px, py, vx.toLong(), vy.toLong())
+}
+
+private fun List<Robot>.calculateSafetyFactor(
+    seconds: Long = 100,
+    width: Int = 101,
+    height: Int = 103,
+    log: Boolean = false,
+): Int {
+    val toPrint = Array(height) { CharArray(width) { '.' } }
+
+    // Define quadrants
+    val midX = width / 2
+    val midY = height / 2
+
+    var q1 = 0
+    var q2 = 0
+    var q3 = 0
+    var q4 = 0
+
+    forEach { robot ->
+        robot.x = (((robot.x + robot.vx * seconds) % width + width) % width).toInt()
+        robot.y = (((robot.y + robot.vy * seconds) % height + height) % height).toInt()
+        toPrint[robot.y][robot.x] = '#'
+        if (robot.x != midX && robot.y != midY) {
+            when {
+                robot.x < midX && robot.y < midY -> q1++
+                robot.x >= midX && robot.y < midY -> q2++
+                robot.x < midX && robot.y >= midY -> q3++
+                else -> q4++
+            }
+        }
+    }
+
+    if (log) {
+        toPrint.forEach { println(String(it)) }
+    }
+
+    return q1 * q2 * q3 * q4
+}
+
+private data class Robot(var x: Int, var y: Int, val vx: Long, val vy: Long)
--- 2024/src/test/kotlin/problem/day14/Day14Test.kt
+++ 2024/src/test/kotlin/problem/day14/Day14Test.kt
@@ -5,19 +5,26 @@ import io.kotest.matchers.shouldBe
 
 class Day14Test : FreeSpec({
 
-    val sampleInput1: List<String> = listOf()
+    val sampleInput1: List<String> = listOf(
+        "p=0,4 v=3,-3",
+        "p=6,3 v=-1,-3",
+        "p=10,3 v=-1,2",
+        "p=2,0 v=2,-1",
+        "p=0,0 v=1,3",
+        "p=3,0 v=-2,-2",
+        "p=7,6 v=-1,-3",
+        "p=3,0 v=-1,-2",
+        "p=9,3 v=2,3",
+        "p=7,3 v=-1,2",
+        "p=2,4 v=2,-3",
+        "p=9,5 v=-3,-3",
+    )
 
-    val sampleSolutionPart1 = -1
-
-    val sampleSolutionPart2 = -1
+    val sampleSolutionPart1 = 12
 
     "Solving day 14" - {
         "part 1 for the sample input should return the correct output" {
-            solveDay14Part1(sampleInput1) shouldBe sampleSolutionPart1
-        }
-
-        "part 2 for the sample input should return the correct output" {
-            solveDay14Part2(sampleInput1) shouldBe sampleSolutionPart2
+            solveDay14Part1(sampleInput1, 11, 7) shouldBe sampleSolutionPart1
         }
     }
 })
