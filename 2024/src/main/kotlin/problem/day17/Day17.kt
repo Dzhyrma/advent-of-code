e58785f66f73cc0dc598ab5be4b7b43c40e155be
Andrii Dzhyrma <sr03a@s-mxs.net>
2024-12-17 09:27:13 +0100

    Add 2024 day 17 solution

--- 2024/src/main/kotlin/problem/day17/Day17.kt
+++ 2024/src/main/kotlin/problem/day17/Day17.kt
@@ -11,10 +11,73 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay17Part1, ::solveDay17Part2)
 }
 
-fun solveDay17Part1(input: List<String>): Long {
-    return TODO()
+fun solveDay17Part1(input: List<String>): String {
+    val a = input.take(3).map { it.split(": ")[1].toLong() }.first()
+    val program = input[4].split(": ")[1].split(",").map { it.toInt() }
+    return program.runProgram(a).joinToString(",")
 }
 
 fun solveDay17Part2(input: List<String>): Long {
-    return TODO()
+    val program = input[4].split(": ")[1].split(",").map { it.toInt() }
+    var a = find_a(program) ?: 0
+    // var a = 0b101_110_001_001_101_011_000_000_000_000_000_000_000_000_000_000
+
+    while (true) {
+        if (program.runProgram(a, expected = program) == program) return a
+        a++
+    }
+}
+
+private fun find_a(program: List<Int>, a: Long = 0): Long? {
+    if (program.isEmpty()) return a
+    for (i in 0..7L) {
+        val newA = (a shl 3) or i
+        val b = i xor 1
+        val d = (b xor (newA shr b.toInt()) xor 4) and 7
+        if (d.toInt() == program.last()) {
+            val result = find_a(program.dropLast(1), newA)
+            if (result != null) return result
+        }
+    }
+    return null
+}
+
+private fun List<Int>.runProgram(aInit: Long, expected: List<Int>? = null): List<Int> {
+    val regs = longArrayOf(aInit, 0, 0)
+    val out = mutableListOf<Int>()
+    var ip = 0
+
+    while (ip < size) {
+        when (this[ip]) {
+            0 -> regs[0] = shiftRightOrZero(regs[0], regs.getComboOperand(this[ip + 1]))
+            1 -> regs[1] = regs[1] xor this[ip + 1].toLong()
+            2 -> regs[1] = regs.getComboOperand(this[ip + 1]) and 7
+            3 -> if (regs[0] != 0L) {
+                ip = this[ip + 1]
+                continue
+            }
+            4 -> regs[1] = regs[1] xor regs[2]
+            5 -> {
+                out.add((regs.getComboOperand(this[ip + 1]) and 7L).toInt())
+                if (expected != null && out[out.size - 1] != expected[out.size - 1]) return emptyList()
+            }
+            6 -> regs[1] = shiftRightOrZero(regs[0], regs.getComboOperand(this[ip + 1]))
+            7 -> regs[2] = shiftRightOrZero(regs[0], regs.getComboOperand(this[ip + 1]))
+        }
+        ip += 2
+    }
+    return out
+}
+
+private fun shiftRightOrZero(a: Long, shift: Long): Long {
+    if (shift > 64) {
+        return 0
+    }
+    return a shr shift.toInt()
+}
+
+private fun LongArray.getComboOperand(operand: Int) = when {
+    operand < 4 -> operand.toLong()
+    operand < 7 -> this[operand - 4]
+    else -> operand.toLong()
 }
--- 2024/src/test/kotlin/problem/day17/Day17Test.kt
+++ 2024/src/test/kotlin/problem/day17/Day17Test.kt
@@ -5,19 +5,33 @@ import io.kotest.matchers.shouldBe
 
 class Day17Test : FreeSpec({
 
-    val sampleInput1: List<String> = listOf()
+    val sampleInput1: List<String> = listOf(
+        "Register A: 729",
+        "Register B: 0",
+        "Register C: 0",
+        "",
+        "Program: 0,1,5,4,3,0",
+    )
 
-    val sampleSolutionPart1 = -1L
+    val sampleInput2: List<String> = listOf(
+        "Register A: 2024",
+        "Register B: 0",
+        "Register C: 0",
+        "",
+        "Program: 0,3,5,4,3,0",
+    )
 
-    val sampleSolutionPart2 = -1L
+    val sampleSolutionPart1_1 = "4,6,3,5,6,3,5,2,1,0"
+
+    val sampleSolutionPart2 = 117440
 
     "Solving day 17" - {
         "part 1 for the sample input should return the correct output" {
-            solveDay17Part1(sampleInput1) shouldBe sampleSolutionPart1
+            solveDay17Part1(sampleInput1) shouldBe sampleSolutionPart1_1
         }
 
         "part 2 for the sample input should return the correct output" {
-            solveDay17Part2(sampleInput1) shouldBe sampleSolutionPart2
+            solveDay17Part2(sampleInput2) shouldBe sampleSolutionPart2
         }
     }
 })
