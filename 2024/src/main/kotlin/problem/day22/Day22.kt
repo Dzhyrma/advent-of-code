41e1d459d63c154575a560ce3359f86915463f78
Andrii Dzhyrma <andrii.dzhyrma@external.erstegroup.com>
2024-12-22 17:03:48 +0100

    Add 2024 day 22 solution

--- 2024/src/main/kotlin/problem/day22/Day22.kt
+++ 2024/src/main/kotlin/problem/day22/Day22.kt
@@ -12,9 +12,44 @@ fun main(args: Array<String>) {
 }
 
 fun solveDay22Part1(input: List<String>): Long {
-    return TODO()
+    return input.map { it.toLong() }.sumOf { generateSecret(it).first }
 }
 
-fun solveDay22Part2(input: List<String>): Long {
-    return TODO()
+fun solveDay22Part2(input: List<String>): Int {
+    val bestSequence = input.findBestSequence()
+    return bestSequence
 }
+
+fun List<String>.findBestSequence(): Int {
+    val sequences = HashMap<Int, Int>(50000, 1f).withDefault { 0 }
+    val subSequences = HashMap<Int, Int>(2001, 1f)
+    map { it.toLong() }.forEach { secret ->
+        val prices = generateSecret(secret).second
+        var seqCode = prices.ch(1) shl 5 or prices.ch(2) shl 5 or prices.ch(3) shl 5 or prices.ch(4)
+        subSequences[seqCode] = prices[4]
+        for (i in 5 until prices.size) {
+            seqCode = seqCode shl 5 or prices.ch(i) and 0xFFFFF
+            if (!subSequences.containsKey(seqCode)) {
+                subSequences[seqCode] = prices[i]
+            }
+        }
+        subSequences.forEach { (seqCode, price) -> sequences[seqCode] = sequences.getValue(seqCode) + price }
+        subSequences.clear()
+    }
+    return sequences.maxOf { it.value }
+}
+
+private fun generateSecret(secret: Long): Pair<Long, List<Int>> {
+    var s = secret
+    val prices = mutableListOf<Int>()
+    prices.add((s % 10L).toInt())
+    repeat(2000) {
+        s = s shl 6 xor s and 0xFFFFFF
+        s = s shr 5 xor s
+        s = s shl 11 xor s and 0xFFFFFF
+        prices.add((s % 10L).toInt())
+    }
+    return s to prices
+}
+
+private fun List<Int>.ch(i: Int) = this[i] - this[i - 1] + 9
--- 2024/src/test/kotlin/problem/day22/Day22Test.kt
+++ 2024/src/test/kotlin/problem/day22/Day22Test.kt
@@ -5,11 +5,23 @@ import io.kotest.matchers.shouldBe
 
 class Day22Test : FreeSpec({
 
-    val sampleInput1: List<String> = listOf()
+    val sampleInput1: List<String> = listOf(
+        "1",
+        "10",
+        "100",
+        "2024",
+    )
 
-    val sampleSolutionPart1 = -1L
+    val sampleInput2: List<String> = listOf(
+        "1",
+        "2",
+        "3",
+        "2024",
+    )
 
-    val sampleSolutionPart2 = -1L
+    val sampleSolutionPart1 = 37327623
+
+    val sampleSolutionPart2 = 23
 
     "Solving day 22" - {
         "part 1 for the sample input should return the correct output" {
@@ -17,7 +29,7 @@ class Day22Test : FreeSpec({
         }
 
         "part 2 for the sample input should return the correct output" {
-            solveDay22Part2(sampleInput1) shouldBe sampleSolutionPart2
+            solveDay22Part2(sampleInput2) shouldBe sampleSolutionPart2
         }
     }
 })
