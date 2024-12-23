d541333cbacd970e5a64e891278a0f0595012707
Andrii Dzhyrma <andrii.dzhyrma@external.erstegroup.com>
2024-12-23 07:05:38 +0100

    Add 2024 day 23 solution

--- 2024/src/main/kotlin/problem/day23/Day23.kt
+++ 2024/src/main/kotlin/problem/day23/Day23.kt
@@ -11,10 +11,33 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay23Part1, ::solveDay23Part2)
 }
 
-fun solveDay23Part1(input: List<String>): Long {
-    return TODO()
+fun solveDay23Part1(input: List<String>): Int {
+    val connections = input.parseInput()
+    return connections.countSetsWithT()
 }
 
-fun solveDay23Part2(input: List<String>): Long {
-    return TODO()
+fun solveDay23Part2(input: List<String>): String {
+    val connections = input.parseInput()
+    return connections.findLanPartyPassword()
 }
+
+private fun List<String>.parseInput() = flatMap { it.split("-").let { (a, b) -> listOf(a to b, b to a) } }
+    .groupBy({ it.first }, { it.second })
+    .mapValues { (_, v) -> v.toSet() }
+    .withDefault { emptySet() }
+
+private fun Map<String, Set<String>>.isConnected(group: Set<String>): Boolean =
+    group.all { node -> this.getValue(node).containsAll(group - node) }
+
+private fun Map<String, Set<String>>.countSetsWithT() = keys
+    .filter { it.startsWith("t") }
+    .flatMap { a -> getValue(a).flatMap { b -> getValue(b).map { c -> setOf(a, b, c) } } }
+    .distinct()
+    .filter { it.size == 3 }
+    .count { isConnected(it) }
+
+private fun Map<String, Set<String>>.findLanPartyPassword() = keys
+    .fold(setOf<String>()) { acc, comp ->
+        val candidateGroup = (acc + comp).toSet()
+        if (isConnected((acc + comp).toSet())) candidateGroup else acc
+    }.sorted().joinToString(",")
--- 2024/src/test/kotlin/problem/day23/Day23Test.kt
+++ 2024/src/test/kotlin/problem/day23/Day23Test.kt
@@ -5,11 +5,18 @@ import io.kotest.matchers.shouldBe
 
 class Day23Test : FreeSpec({
 
-    val sampleInput1: List<String> = listOf()
+    val sampleInput1: List<String> = listOf(
+        "ka-co",
+        "ta-co",
+        "de-co",
+        "ta-ka",
+        "de-ta",
+        "ka-de",
+    )
 
     val sampleSolutionPart1 = -1L
 
-    val sampleSolutionPart2 = -1L
+    val sampleSolutionPart2 = "co,de,ka,ta"
 
     "Solving day 23" - {
         "part 1 for the sample input should return the correct output" {
